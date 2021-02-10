c790eaaa1f1a29477061b96cc3c35dfe01d81ba4
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-02-10 16:14:48 +0000

    deleted everything and restarted with axios

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-
-var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const PORT = 5000;
-
-app.listen(PORT, function () {
-    console.log('Server listening on http://localhost:' + PORT);
-});
+const axios = require("axios");
