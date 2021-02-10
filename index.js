c9501fb83a042825ffc55b9ddcd755e2d0508cde
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-02-10 16:04:56 +0000

    added body-parser middleware

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = 5000;
+
+app.listen(PORT, function () {
+    console.log('Server listening on http://localhost:' + PORT);
+});
