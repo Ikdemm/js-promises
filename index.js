c314412257d2706ad32cc135afade9f60716fef4
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-02-10 16:38:30 +0000

    getting back the emojis from github API

--- index.js
+++ index.js
@@ -2,5 +2,20 @@ const axios = require("axios");
 const API_TOKEN = require("./api.token")
 
 function getEmojis() {
-
+    axios.get(
+        "https://api.github.com/emojis",
+        {
+            headers: {
+                'Authorization': `token ${API_TOKEN}`
+            }
+        })
+        .then((data) => {
+            console.log(data)
+            res.send(data)
+        })
+        .catch((err) => {
+            throw err
+        })
 }
+
+getEmojis()
